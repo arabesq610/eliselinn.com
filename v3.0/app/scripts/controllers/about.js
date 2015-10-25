5089cf3b110f446096a1b8786e43fbfab30e1fb5
Elise Linn <elise@gofastpay.com>
2015-10-25 15:14:38 -0700

    initial commit of v3

---
+++ v3.0/app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name v30App.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the v30App
+ */
+angular.module('v30App')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ v3.0/app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name v30App.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the v30App
+ */
+angular.module('v30App')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
